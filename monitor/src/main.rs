44f298acf7a8840f55f4f827fccbaff59b892ec2
Delan Azabani <dazabani@igalia.com>
2024-07-23 22:00:33 +0800

    monitor: list runner volumes

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -40,6 +40,7 @@ fn main() -> eyre::Result<()> {
 
     dbg!(list_registered_runners_for_host()?);
     dbg!(list_runner_guests()?);
+    dbg!(list_runner_volumes()?);
 
     Ok(())
 }
@@ -79,6 +80,28 @@ fn list_runner_guests() -> eyre::Result<Vec<String>> {
     }
 
     let prefix = format!("{}-", env::var("SERVO_CI_LIBVIRT_PREFIX")?);
+    let result = str::from_utf8(&output.stdout)
+        .wrap_err("Failed to decode UTF-8")?
+        .split_terminator('\n')
+        .filter(|name| name.starts_with(&prefix))
+        .map(str::to_owned);
+
+    Ok(result.collect())
+}
+
+fn list_runner_volumes() -> eyre::Result<Vec<String>> {
+    let output = Command::new("../list-runner-volumes.sh")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap()
+        .wait_with_output()
+        .unwrap();
+    if !output.status.success() {
+        eyre::bail!("Command exited with status {}", output.status);
+    }
+
+    // Output is already filtered by prefix, but filter again just in case.
+    let prefix = format!("{}/", env::var("SERVO_CI_ZFS_PREFIX")?);
     let result = str::from_utf8(&output.stdout)
         .wrap_err("Failed to decode UTF-8")?
         .split_terminator('\n')
