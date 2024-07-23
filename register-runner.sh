bf99a2d4474740785d0eccb95291d23901745844
Delan Azabani <dazabani@igalia.com>
2024-07-23 20:05:17 +0800

    Add suffix to GitHub API runner names

--- register-runner.sh
+++ register-runner.sh
@@ -8,5 +8,5 @@ vm=$1; shift
 
 gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
     "$SERVO_CI_GITHUB_API_SCOPE/actions/runners/generate-jitconfig" \
-    -f "name=$vm" -F "runner_group_id=1" -f 'work_folder='"$work_folder" \
+    -f "name=$vm@$SERVO_CI_GITHUB_API_SUFFIX" -F "runner_group_id=1" -f 'work_folder='"$work_folder" \
     -f "labels[]=self-hosted" -f "labels[]=X64" -f "labels[]=$platform_label"
