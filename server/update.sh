d611f35ff770f884d0434fb49034c95629c22388
Delan Azabani <dazabani@igalia.com>
2024-10-31 12:58:46 +0800

    server: make update.sh symlink the NixOS config, not copy it

--- server/update.sh
+++ server/update.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 # usage: update.sh
 # requires: git nixos-rebuild
-if [ $# -lt 1 ]; then >&2 sed '2!d;2s/^# //;2q' "$0"; exit 1; fi
 set -xeuo pipefail -o bsdecho
 script_dir=${0:a:h}
 nixos_dir=$script_dir/nixos
 
 cd "$script_dir"
 git pull
-cp -R "$nixos_dir" /etc
+rm -Rf /etc/nixos
+ln -sr "$nixos_dir" /etc/nixos
 nixos-rebuild switch
