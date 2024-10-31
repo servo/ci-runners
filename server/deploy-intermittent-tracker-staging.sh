e5cf6693074f18bcfb257bbdb52aa150538ed9e2
Delan Azabani <dazabani@igalia.com>
2024-10-31 13:17:36 +0800

    server: add deploy-intermittent-tracker-staging.sh

---
+++ server/deploy-intermittent-tracker-staging.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+set -xeuo pipefail -o bsdecho
+script_dir=${0:a:h}
+cd "$script_dir/../intermittent-tracker"
+
+systemctl stop intermittent-tracker-staging
+cp -v prod/data/* staging/data/
+systemctl start intermittent-tracker-staging
