73806546f844ccac756afe63410d42ec79635a04
Delan Azabani <dazabani@igalia.com>
2024-11-01 13:28:38 +0800

    server: move deploy-intermittent-tracker-staging.sh to subdirectory

--- server/intermittent-tracker/deploy-staging.sh
+++ server/intermittent-tracker/deploy-staging.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 set -xeuo pipefail -o bsdecho
 script_dir=${0:a:h}
-cd "$script_dir/../intermittent-tracker"
+cd "$script_dir/../../intermittent-tracker"
 
 systemctl stop intermittent-tracker-staging
 cp -v prod/data/* staging/data/
