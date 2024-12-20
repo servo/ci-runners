c4c2534bb758dc720d35c32ef690bf6e7512f960
Delan Azabani <dazabani@igalia.com>
2024-12-20 15:32:19 +0800

    server: rebuild monitor in deploy.sh

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,6 +5,8 @@ set -xeuo pipefail -o bsdecho
 script_dir=${0:a:h}
 nixos_dir=$script_dir/nixos
 
+cd "$script_dir"
+( cd ../monitor && cargo build )
 rm -Rf /etc/nixos
 ln -sr "$nixos_dir" /etc/nixos
 nixos-rebuild switch
