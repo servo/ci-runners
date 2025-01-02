9f7d3425fa01000c00c380f225b2a8c5bb2a5509
Delan Azabani <dazabani@igalia.com>
2025-01-02 16:09:44 +0800

    monitor: initial support for fully automated image rebuilds (#6)

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -9,25 +9,29 @@ mod settings;
 mod shell;
 mod zfs;
 
+use core::str;
 use std::{
     collections::BTreeMap,
     fs::File,
-    io::Read,
+    io::{ErrorKind, Read},
+    mem::take,
     net::IpAddr,
     path::Path,
     process::exit,
     sync::{LazyLock, RwLock},
-    thread::{self},
+    thread::{self, JoinHandle},
     time::{Duration, UNIX_EPOCH},
 };
 
 use askama::Template;
+use chrono::{SecondsFormat, Utc};
 use crossbeam_channel::{Receiver, Sender};
 use dotenv::dotenv;
 use http::StatusCode;
-use jane_eyre::eyre::{self, eyre, Context};
+use jane_eyre::eyre::{self, bail, eyre, Context, OptionExt};
 use mktemp::Temp;
 use serde_json::json;
+use subprocess::{CommunicateError, Exec, Redirection};
 use tracing::{error, info, trace, warn};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 use warp::{
@@ -372,8 +376,9 @@ fn monitor_thread() -> eyre::Result<()> {
         IdGen::new_empty()
     });
 
-    let /* mut */ profiles = TOML.initial_profiles();
+    let mut profiles = TOML.initial_profiles();
     let mut registrations_cache = Cache::default();
+    let mut image_rebuilds = BTreeMap::default();
 
     loop {
         let registrations = registrations_cache.get(|| list_registered_runners_for_host())?;
@@ -407,13 +412,18 @@ fn monitor_thread() -> eyre::Result<()> {
             },
         ) in profile_runner_counts.iter()
         {
-            info!("profile {key}: image age {image_age:?}, {healthy}/{target} healthy runners ({idle} idle, {reserved} reserved, {busy} busy, {started_or_crashed} started or crashed, {excess_idle} excess idle, {wanted} wanted)");
+            let profile = profiles.get(key).ok_or_eyre("Failed to get profile")?;
+            info!("profile {key}: {healthy}/{target} healthy runners ({idle} idle, {reserved} reserved, {busy} busy, {started_or_crashed} started or crashed, {excess_idle} excess idle, {wanted} wanted), image {}/{}@{} age {image_age:?}", DOTENV.zfs_clone_prefix, profile.base_vm_name, profile.base_image_snapshot);
         }
         for (_id, runner) in runners.iter() {
             runner.log_info();
         }
 
-        // Determine whether any profiles need their images rebuilt.
+        // Kick off rebuilds for any profiles whose images are too old.
+        struct Rebuild {
+            thread: JoinHandle<eyre::Result<()>>,
+            snapshot_name: String,
+        }
         for (key, profile) in profiles.iter() {
             let needs_rebuild = profile.image_needs_rebuild();
             if needs_rebuild.unwrap_or(true) {
@@ -428,14 +438,51 @@ fn monitor_thread() -> eyre::Result<()> {
                         key,
                         runner_count, "profile image needs rebuild; waiting for runners"
                     );
+                } else if image_rebuilds.contains_key(key) {
+                    info!(
+                        key,
+                        runner_count, "profile image needs rebuild; image rebuild still running"
+                    );
                 } else {
                     info!(
                         key,
-                        runner_count, "profile image needs rebuild; TODO start image rebuild"
+                        runner_count, "profile image needs rebuild; starting image rebuild now"
                     );
+                    let build_script_path =
+                        Path::new(&profile.configuration_name).join("build-image.sh");
+                    let snapshot_name = Utc::now().to_rfc3339_opts(SecondsFormat::Nanos, true);
+                    let cloned_snapshot_name = snapshot_name.clone();
+                    let rebuild = Rebuild {
+                        thread: thread::spawn(move || {
+                            image_rebuild_thread(build_script_path, &cloned_snapshot_name)
+                        }),
+                        snapshot_name: snapshot_name.clone(),
+                    };
+                    image_rebuilds.insert(key.to_owned(), rebuild);
                 }
             }
         }
+
+        // Reap image rebuild threads, updating the profile on success.
+        let mut remaining_image_rebuilds = BTreeMap::default();
+        for (profile_key, rebuild) in take(&mut image_rebuilds) {
+            if rebuild.thread.is_finished() {
+                match rebuild.thread.join() {
+                    Ok(Ok(())) => {
+                        info!(profile_key, "Image rebuild thread exited");
+                        let profile = profiles
+                            .get_mut(&profile_key)
+                            .ok_or_eyre("Failed to get profile")?;
+                        profile.base_image_snapshot = rebuild.snapshot_name;
+                    }
+                    Ok(Err(report)) => error!(profile_key, %report, "Image rebuild thread error"),
+                    Err(panic) => error!(profile_key, ?panic, "Image rebuild thread panic"),
+                };
+            } else {
+                remaining_image_rebuilds.insert(profile_key, rebuild);
+            }
+        }
+        image_rebuilds.extend(remaining_image_rebuilds);
 
         let mut unregister_and_destroy = |id, runner: &Runner| {
             if runner.registration().is_some() {
@@ -577,4 +624,53 @@ fn monitor_thread() -> eyre::Result<()> {
             info!("Did not receive an API request");
         }
     }
+}
+
+#[tracing::instrument(skip(build_script_path), fields(build_script_path = ?build_script_path.as_ref()))]
+fn image_rebuild_thread(
+    build_script_path: impl AsRef<Path>,
+    snapshot_name: &str,
+) -> eyre::Result<()> {
+    let mut child = Exec::cmd(build_script_path.as_ref())
+        .cwd("..")
+        .arg(snapshot_name)
+        .stdout(Redirection::Pipe)
+        .stderr(Redirection::Merge)
+        .popen()?;
+    let mut communicator = child
+        .communicate_start(None)
+        .limit_time(Duration::from_secs(1));
+    let exit_status = loop {
+        match communicator.read() {
+            Err(error) if error.kind() != ErrorKind::TimedOut => {
+                warn!(?error, "Error reading from child process");
+                break child.wait()?;
+            }
+            // Err(empty) or Err(non-empty) means we timed out, and there may be more output in future.
+            // Ok(non-empty) means we got some output. Hopefully this avoids giving us partial lines?
+            // Ok(empty) means there is definitely no more output.
+            ref result @ (Ok(ref capture) | Err(CommunicateError { ref capture, .. })) => {
+                let (Some(stdout), None) = capture else {
+                    unreachable!("Guaranteed by child definition")
+                };
+                if result.is_ok() && stdout.is_empty() {
+                    // There is definitely no more output
+                    break child.wait()?;
+                } else if !stdout.is_empty() {
+                    for line in stdout.split(|&b| b == b'\n') {
+                        let line = str::from_utf8(line).map_err(|_| line);
+                        match line {
+                            Ok(string) => info!(line = %string),
+                            Err(bytes) => info!(?bytes),
+                        }
+                    }
+                }
+            }
+        }
+    };
+    if !exit_status.success() {
+        bail!("Command exited with status {:?}", exit_status);
+    }
+
+    Ok(())
 }
