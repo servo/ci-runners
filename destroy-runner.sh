68a98f415210e4f0ed56ca595cf01170c1f70afb
Delan Azabani <dazabani@igalia.com>
2025-01-07 13:48:50 +0800

    macos13: add support for image in destroy-runner.sh (#3)

--- destroy-runner.sh
+++ destroy-runner.sh
@@ -7,5 +7,5 @@ vm=$base_vm.$1; shift
 libvirt_vm=$SERVO_CI_LIBVIRT_PREFIX-$vm
 
 virsh destroy $libvirt_vm || :
-virsh undefine $libvirt_vm || :
+virsh undefine --nvram $libvirt_vm || :
 zfs destroy -v $SERVO_CI_ZFS_PREFIX/$vm
