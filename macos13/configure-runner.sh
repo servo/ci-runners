7998625e122f6714431278aaf690e76274dbc793
Delan Azabani <dazabani@igalia.com>
2025-01-07 15:06:53 +0800

    macos13: oops, unpack runner in guest, not on host (#3)

--- macos13/configure-runner.sh
+++ macos13/configure-runner.sh
@@ -6,13 +6,10 @@ cache_dir=$script_dir/cache
 runner_jitconfig=$1; shift
 vm=$1; shift
 
->&2 echo '[*] Unpacking GitHub Actions runner'
-mount_dir=$(pwd)
-mkdir -p ~/actions-runner
-cd ~/actions-runner
-tar xf "$mount_dir/init/actions-runner-osx-x64.tar.gz"
-
 >&2 echo '[*] Injecting GitHub Actions config'
 # Register the runner as both ephemeral (one job only) and just-in-time (no token required)
 # See also: <https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions#using-just-in-time-runners>
-> "$mount_dir/init/runner.sh" echo '~/actions-runner/run.sh --jitconfig '"$runner_jitconfig"
+> init/runner.sh echo 'mkdir -p ~/actions-runner'
+>> init/runner.sh echo 'cd ~/actions-runner'
+>> init/runner.sh echo 'tar xf /Volumes/a/init/actions-runner-osx-x64.tar.gz'
+>> init/runner.sh echo '~/actions-runner/run.sh --jitconfig '"$runner_jitconfig"
