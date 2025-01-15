536062be2a5574cabb7908747a55d08388daaef3
Delan Azabani <dazabani@igalia.com>
2025-01-15 13:15:33 +0800

    server: fix experimental features error in setup scripts

--- server/build-nixos-installer-kexec.sh
+++ server/build-nixos-installer-kexec.sh
@@ -9,6 +9,9 @@ cat <<'EOF' > "$config_file"
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBvkS7z2RAWzqRByRsHHB8PoCjXrnyHtjpdTxmOdcom delan@azabani.com/2016-07-18/Ed25519"];
   environment.systemPackages = with pkgs; [ git zsh jq ];
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 }
 EOF
 nix run github:nix-community/nixos-generators/74b8e31dd709760c86eed16b6c1d0b88d7360937 -- -o result -f kexec-bundle -c "$config_file"
--- server/start-nixos-installer.sh
+++ server/start-nixos-installer.sh
@@ -13,6 +13,9 @@ cat <<'EOF' > /root/config.nix
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBvkS7z2RAWzqRByRsHHB8PoCjXrnyHtjpdTxmOdcom delan@azabani.com/2016-07-18/Ed25519"];
   environment.systemPackages = with pkgs; [ git zsh jq ];
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 }
 EOF
 nix --extra-experimental-features 'nix-command flakes' run \
