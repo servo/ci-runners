841fac9e8ccbafc1a30704ace8b32492e34b6847
Delan Azabani <dazabani@igalia.com>
2025-01-15 12:50:44 +0800

    server: don’t copy server/nixos into machine /etc/nixos

--- server/install-or-reinstall.sh
+++ server/install-or-reinstall.sh
@@ -8,9 +8,6 @@ nixos_dir=$script_dir/nixos
 mnt_dir=$1
 hostname=$2
 
-mkdir -p "$mnt_dir/etc"
-cp -R "$nixos_dir" "$mnt_dir/etc"
-
 # Like `nixos-install --flake .\#$hostname`, but avoids the error in NixOS/nix#4081:
 # <https://github.com/NixOS/nix/issues/4081#issuecomment-753237142>
 cd "$nixos_dir"
