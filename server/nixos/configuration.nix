abe09b03c8b22312238ed41dfcba5e898795a0fe
Delan Azabani <dazabani@igalia.com>
2025-01-15 18:34:08 +0800

    server: remove @delan’s root@jupiter ssh key

--- server/nixos/configuration.nix
+++ server/nixos/configuration.nix
@@ -121,7 +121,6 @@
   programs.mosh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBvkS7z2RAWzqRByRsHHB8PoCjXrnyHtjpdTxmOdcom delan@azabani.com/2016-07-18/Ed25519"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXIh9BXwJRhFyupR6OGMT11vzXFf3igCng+nisSK3Px root@jupiter@delan@azabani.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPag2UMaWyIEIsL0EbdvChBCcARVxNeJAplUZe70kXlr mrobinson@igalia.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIPiTpwVkwvE8npI7Z944tmrFlPKDjDVbm1rI7m4lng7 me@mukilan"
   ];
