ffac9501a54c3fa6bfa21d5fd6fa4856e6ef3a4c
Delan Azabani <dazabani@igalia.com>
2025-01-15 17:16:04 +0800

    Bump hash for uv install.sh

--- macos13/configure-base.sh
+++ macos13/configure-base.sh
@@ -9,7 +9,7 @@ script_dir=${0:a:h}/..
 mkdir -p -- "$SERVO_CI_CACHE_PATH"
 download "$SERVO_CI_CACHE_PATH" https://static.rust-lang.org/rustup/rustup-init.sh 32a680a84cf76014915b3f8aa44e3e40731f3af92cd45eb0fcc6264fd257c428
 download "$SERVO_CI_CACHE_PATH" https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-osx-x64-2.321.0.tar.gz b2c91416b3e4d579ae69fc2c381fc50dbda13f1b3fcc283187e2c75d1b173072
-download "$SERVO_CI_CACHE_PATH" https://astral.sh/uv/install.sh 47ead06f79eba7461fd113fc92dc0f191af7455418462fbbed21affa2a6c22e2
+download "$SERVO_CI_CACHE_PATH/uv" https://astral.sh/uv/install.sh 388b22971ac90864ce4b12a209014ae99b16088a80a107fab1c01f0c9e50751d
 download "$SERVO_CI_CACHE_PATH" https://raw.githubusercontent.com/actions/runner-images/3d5f09a90fd475a3531b0ef57325aa7e27b24595/images/macos/scripts/build/install-xcode-clt.sh 2c90d2c76f2d375ef5404d67510894ebbf917940aef5017ebee4c6b8c10c42fb
 download "$SERVO_CI_CACHE_PATH/homebrew" https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh 9ad0c8048f3f1a01d5f6610e0df347ceeae5879cf0aa51c1d987aa8aee740dca
 
@@ -17,7 +17,7 @@ download "$SERVO_CI_CACHE_PATH/homebrew" https://raw.githubusercontent.com/Homeb
 mkdir -p init
 inject_exfat init "$script_dir/macos13/init.sh"
 inject_exfat init "$SERVO_CI_CACHE_PATH/rustup-init.sh"
-inject_exfat init/install-uv.sh "$SERVO_CI_CACHE_PATH/install.sh"
+inject_exfat init/install-uv.sh "$SERVO_CI_CACHE_PATH/uv/install.sh"
 inject_exfat init "$SERVO_CI_CACHE_PATH/install-xcode-clt.sh"
 inject_exfat init/install-homebrew.sh "$SERVO_CI_CACHE_PATH/homebrew/install.sh"
 chmod +x init/rustup-init.sh init/install-uv.sh init/install-xcode-clt.sh init/install-homebrew.sh
--- ubuntu2204/configure-base.sh
+++ ubuntu2204/configure-base.sh
@@ -9,13 +9,13 @@ script_dir=${0:a:h}/..
 mkdir -p -- "$SERVO_CI_CACHE_PATH"
 download "$SERVO_CI_CACHE_PATH" https://static.rust-lang.org/rustup/rustup-init.sh 32a680a84cf76014915b3f8aa44e3e40731f3af92cd45eb0fcc6264fd257c428
 download "$SERVO_CI_CACHE_PATH" https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz ba46ba7ce3a4d7236b16fbe44419fb453bc08f866b24f04d549ec89f1722a29e
-download "$SERVO_CI_CACHE_PATH" https://astral.sh/uv/install.sh 47ead06f79eba7461fd113fc92dc0f191af7455418462fbbed21affa2a6c22e2
+download "$SERVO_CI_CACHE_PATH/uv" https://astral.sh/uv/install.sh 388b22971ac90864ce4b12a209014ae99b16088a80a107fab1c01f0c9e50751d
 
 >&2 echo '[*] Injecting init script'
 mkdir -p init
 inject etc/rc.local "$script_dir/ubuntu2204/init.sh"
 inject init "$SERVO_CI_CACHE_PATH/rustup-init.sh"
-inject init/install-uv.sh "$SERVO_CI_CACHE_PATH/install.sh"
+inject init/install-uv.sh "$SERVO_CI_CACHE_PATH/uv/install.sh"
 chmod +x init/rustup-init.sh init/install-uv.sh
 
 >&2 echo '[*] Injecting GitHub Actions runner'
