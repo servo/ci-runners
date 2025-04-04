4d8a563d7df3e608401e62cc8697c1d138bc57db
Delan Azabani <dazabani@igalia.com>
2025-04-04 13:06:36 +0800

    monitor: add log_output_as_trace

--- monitor/src/shell.rs
+++ monitor/src/shell.rs
@@ -12,7 +12,7 @@ use std::{
 
 use jane_eyre::eyre::{self, Context};
 use mktemp::Temp;
-use tracing::{debug, info, warn};
+use tracing::{debug, info, trace, warn};
 
 /// Global instance of [Shell] for single-threaded situations.
 pub static SHELL: LazyLock<Mutex<Shell>> =
@@ -81,57 +81,64 @@ impl DerefMut for ShellHandle<'_> {
     }
 }
 
-/// Log the given output to tracing.
-///
-/// Unlike cmd_lib’s built-in logging:
-/// - it handles CR-based progress output correctly, such as in `curl`, `dd`, and `rsync`
-/// - it uses `tracing` instead of `log`, so the logs show the correct target and any span context
-///   given via `#[tracing::instrument]`
-///
-/// This only works with `spawn_with_output!()`, and `wait_with_pipe()` only works with stdout, so
-/// if you want to log both stdout and stderr, use `spawn_with_output!(... 2>&1)`.
-pub fn log_output_as_info(reader: Box<dyn Read>) {
-    let mut reader = BufReader::new(reader);
-    let mut buffer = vec![];
-    loop {
-        // Unconditionally try to read more data, since the BufReader buffer is empty
-        let result = match reader.fill_buf() {
-            Ok(buffer) => buffer,
-            Err(error) => {
-                warn!(?error, "Error reading from child process");
-                break;
+macro_rules! impl_log_output_as {
+    ($name:ident, $macro:ident) => {
+        /// Log the given output to tracing.
+        ///
+        /// Unlike cmd_lib’s built-in logging:
+        /// - it handles CR-based progress output correctly, such as in `curl`, `dd`, and `rsync`
+        /// - it uses `tracing` instead of `log`, so the logs show the correct target and any span context
+        ///   given via `#[tracing::instrument]`
+        ///
+        /// This only works with `spawn_with_output!()`, and `wait_with_pipe()` only works with stdout, so
+        /// if you want to log both stdout and stderr, use `spawn_with_output!(... 2>&1)`.
+        pub fn $name(reader: Box<dyn Read>) {
+            let mut reader = BufReader::new(reader);
+            let mut buffer = vec![];
+            loop {
+                // Unconditionally try to read more data, since the BufReader buffer is empty
+                let result = match reader.fill_buf() {
+                    Ok(buffer) => buffer,
+                    Err(error) => {
+                        warn!(?error, "Error reading from child process");
+                        break;
+                    }
+                };
+                // Add the result onto our own buffer
+                buffer.extend(result);
+                // Empty the BufReader
+                let read_len = result.len();
+                reader.consume(read_len);
+
+                // Log output to tracing. Take whole “lines” at every LF or CR (for progress bars etc),
+                // but leave any incomplete lines in our buffer so we can try to complete them.
+                while let Some(offset) = buffer.iter().position(|&b| b == b'\n' || b == b'\r') {
+                    let line = &buffer[..offset];
+                    let line = str::from_utf8(line).map_err(|_| line);
+                    match line {
+                        Ok(string) => $macro!(line = %string),
+                        Err(bytes) => $macro!(?bytes),
+                    }
+                    buffer = buffer.split_off(offset + 1);
+                }
+
+                if read_len == 0 {
+                    break;
+                }
             }
-        };
-        // Add the result onto our own buffer
-        buffer.extend(result);
-        // Empty the BufReader
-        let read_len = result.len();
-        reader.consume(read_len);
 
-        // Log output to tracing. Take whole “lines” at every LF or CR (for progress bars etc),
-        // but leave any incomplete lines in our buffer so we can try to complete them.
-        while let Some(offset) = buffer.iter().position(|&b| b == b'\n' || b == b'\r') {
-            let line = &buffer[..offset];
-            let line = str::from_utf8(line).map_err(|_| line);
-            match line {
-                Ok(string) => info!(line = %string),
-                Err(bytes) => info!(?bytes),
+            // Log any remaining incomplete line to tracing.
+            if !buffer.is_empty() {
+                let line = &buffer;
+                let line = str::from_utf8(line).map_err(|_| line);
+                match line {
+                    Ok(string) => $macro!(line = %string),
+                    Err(bytes) => $macro!(?bytes),
+                }
             }
-            buffer = buffer.split_off(offset + 1);
         }
-
-        if read_len == 0 {
-            break;
-        }
-    }
-
-    // Log any remaining incomplete line to tracing.
-    if !buffer.is_empty() {
-        let line = &buffer;
-        let line = str::from_utf8(line).map_err(|_| line);
-        match line {
-            Ok(string) => info!(line = %string),
-            Err(bytes) => info!(?bytes),
-        }
-    }
+    };
 }
+
+impl_log_output_as!(log_output_as_trace, trace);
+impl_log_output_as!(log_output_as_info, info);
