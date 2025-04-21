18a2775fd74b1e4082eb44eec346ed7f01560ef6
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2025-04-21 09:02:29 +0200

    Minor updates to docker hos images (#33)
    - Fix permissions for ohos-config.tar
    - Bump dependencies (uv, github runner, commandline tools)
    - Download command line tools automatically.

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,9 @@
 set -eu
 
 SERVO_GIT_HASH=$(git ls-remote https://github.com/servo/servo.git --branches refs/heads/main | awk '{ print $1}')
-HOS_COMMANDLINE_TOOLS_VERSION="5.0.3.906"
-GITHUB_ACTIONS_RUNNER_VERSION="2.322.0"
+GITHUB_ACTIONS_RUNNER_VERSION="2.323.0"
 RUST_VERSION="1.85.0"
-UV_VERSION="0.6.4"
+UV_VERSION="0.6.14"
 IMAGE_USERNAME=servo_ci
 
 
@@ -16,20 +15,12 @@ then
     cd hos_builder && wget https://cli.github.com/packages/githubcli-archive-keyring.gpg && cd -
 fi
 
-if [[ ! -f "hos_commandline_tools/commandline-tools-linux-x64-${HOS_COMMANDLINE_TOOLS_VERSION}.zip" ]]
-then
-    echo "Couldn't find the HarmonyOS commandline tools (version ${HOS_COMMANDLINE_TOOLS_VERSION}). Please download manually."
-    echo "The commandline tools can be found at https://developer.huawei.com/consumer/cn/download/"
-    echo "Note: Currently the commandline tools are only available to developers with a mainland china huawei account."
-    exit 1
-fi
-
 # Build the helper images
 docker build base -f base/Dockerfile -t servo_gha_base:latest --build-arg=USERNAME=${IMAGE_USERNAME}
 docker build gh_runner -f gh_runner/Dockerfile -t "servo_gha_runner:${GITHUB_ACTIONS_RUNNER_VERSION}" \
     --build-arg=USERNAME=${IMAGE_USERNAME} \
     --build-arg=GITHUB_ACTIONS_RUNNER_VERSION=${GITHUB_ACTIONS_RUNNER_VERSION}
-docker build hos_commandline_tools -f hos_commandline_tools/Dockerfile -t "hos_commandline_tools:${HOS_COMMANDLINE_TOOLS_VERSION}" \
+docker build hos_commandline_tools -f hos_commandline_tools/Dockerfile -t "hos_commandline_tools" \
    --build-arg=USERNAME=${IMAGE_USERNAME}
 
 # Build the actual images
