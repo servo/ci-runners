88b1ff157444ff2be6fee324a3c47f33c1592be1
Delan Azabani <delan@azabani.com>
2025-07-16 15:52:39 +0800

    monitor: settings stubs for tests

--- monitor/settings/src/lib.rs
+++ monitor/settings/src/lib.rs
@@ -11,7 +11,6 @@ use std::{
     time::Duration,
 };
 
-use dotenv::dotenv;
 use jane_eyre::eyre::{self, bail};
 use serde::Deserialize;
 
@@ -31,13 +30,25 @@ pub static IMAGE_DEPS_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
 });
 
 pub static DOTENV: LazyLock<Dotenv> = LazyLock::new(|| {
-    dotenv().expect("Failed to load variables from .env");
-    Dotenv::load()
+    #[cfg(not(any(test, feature = "test")))]
+    // FIXME: ensure that this is called before any other threads are started.
+    // If we can’t ensure that, remove this and spawn ourselves as a child with a modified env.
+    // <https://github.com/dotenv-rs/dotenv/issues/99>
+    dotenv::dotenv().expect("Failed to load variables from .env");
+    #[cfg(not(any(test, feature = "test")))]
+    return Dotenv::load();
+    #[cfg(any(test, feature = "test"))]
+    return Dotenv::load_for_tests();
 });
 
-pub static TOML: LazyLock<Toml> =
-    LazyLock::new(|| Toml::load_default().expect("Failed to load settings from monitor.toml"));
+pub static TOML: LazyLock<Toml> = LazyLock::new(|| {
+    #[cfg(not(any(test, feature = "test")))]
+    return Toml::load_default().expect("Failed to load settings from monitor.toml");
+    #[cfg(any(test, feature = "test"))]
+    return Toml::load_for_tests().expect("Guaranteed by monitor.toml.example");
+});
 
+#[derive(Default)]
 pub struct Dotenv {
     // GITHUB_TOKEN not used
     // LIBVIRT_DEFAULT_URI not used
@@ -70,12 +81,10 @@ pub struct Toml {
 impl Dotenv {
     pub fn load() -> Self {
         let monitor_api_token = env_string("SERVO_CI_MONITOR_API_TOKEN");
-        if monitor_api_token == "ChangeMe" {
-            panic!("SERVO_CI_MONITOR_API_TOKEN must be changed!");
-        }
-
-        Self {
-            monitor_api_token_authorization_value: format!("Bearer {monitor_api_token}"),
+        let result = Self {
+            monitor_api_token_authorization_value: Self::monitor_api_token_authorization_value(
+                &monitor_api_token,
+            ),
             github_api_scope: env_string("SERVO_CI_GITHUB_API_SCOPE"),
             github_api_suffix: env_string("SERVO_CI_GITHUB_API_SUFFIX"),
             libvirt_prefix: env_string("SERVO_CI_LIBVIRT_PREFIX"),
@@ -90,7 +99,120 @@ impl Dotenv {
             dont_register_runners: env_bool("SERVO_CI_DONT_REGISTER_RUNNERS"),
             dont_create_runners: env_bool("SERVO_CI_DONT_CREATE_RUNNERS"),
             main_repo_path: env_string("SERVO_CI_MAIN_REPO_PATH"),
+        };
+
+        result.validate()
+    }
+
+    #[cfg(any(test, feature = "test"))]
+    fn load_for_tests() -> Self {
+        let mut github_api_scope = None;
+        let mut github_api_suffix = None;
+        let mut libvirt_prefix = None;
+        let mut monitor_data_path = None;
+        let mut monitor_poll_interval = None;
+        let mut api_cache_timeout = None;
+        let mut monitor_start_timeout = None;
+        let mut monitor_reserve_timeout = None;
+        let mut monitor_thread_send_timeout = None;
+        let mut monitor_thread_recv_timeout = None;
+        let mut destroy_all_non_busy_runners = None;
+        let mut dont_register_runners = None;
+        let mut dont_create_runners = None;
+        let mut main_repo_path = None;
+
+        // TODO: find a way to do this without a temporary file
+        use std::io::Write;
+        let env_path = mktemp::Temp::new_path();
+        File::create_new(&env_path)
+            .expect("Failed to create temporary env file")
+            .write_all(include_bytes!("../../../.env.example"))
+            .expect("Failed to write temporary env file");
+
+        // TODO: this is no longer marked deprecated, but a new version of dotenv has not been released.
+        // Remove this allow once we’ve updated to that new version of dotenv.
+        #[allow(deprecated)]
+        for entry in dotenv::from_path_iter(env_path).expect("Failed to load temporary env file") {
+            let (key, value) = entry.expect("Failed to load entry");
+            match &*key {
+                "SERVO_CI_MONITOR_API_TOKEN" => { /* do nothing (see below) */ }
+                "SERVO_CI_GITHUB_API_SCOPE" => github_api_scope = Some(value),
+                "SERVO_CI_GITHUB_API_SUFFIX" => github_api_suffix = Some(value),
+                "SERVO_CI_LIBVIRT_PREFIX" => libvirt_prefix = Some(value),
+                "SERVO_CI_MONITOR_DATA_PATH" => monitor_data_path = Some(value),
+                "SERVO_CI_MONITOR_POLL_INTERVAL" => monitor_poll_interval = Some(value),
+                "SERVO_CI_API_CACHE_TIMEOUT" => api_cache_timeout = Some(value),
+                "SERVO_CI_MONITOR_START_TIMEOUT" => monitor_start_timeout = Some(value),
+                "SERVO_CI_MONITOR_RESERVE_TIMEOUT" => monitor_reserve_timeout = Some(value),
+                "SERVO_CI_MONITOR_THREAD_SEND_TIMEOUT" => monitor_thread_send_timeout = Some(value),
+                "SERVO_CI_MONITOR_THREAD_RECV_TIMEOUT" => monitor_thread_recv_timeout = Some(value),
+                "SERVO_CI_DESTROY_ALL_NON_BUSY_RUNNERS" => {
+                    destroy_all_non_busy_runners = Some(value)
+                }
+                "SERVO_CI_DONT_REGISTER_RUNNERS" => dont_register_runners = Some(value),
+                "SERVO_CI_DONT_CREATE_RUNNERS" => dont_create_runners = Some(value),
+                "SERVO_CI_MAIN_REPO_PATH" => main_repo_path = Some(value),
+                _ => { /* do nothing */ }
+            }
         }
+
+        // Totally not `ChangeMe`.
+        let monitor_api_token = "ChangedMe";
+
+        let result = Self {
+            monitor_api_token_authorization_value: Self::monitor_api_token_authorization_value(
+                monitor_api_token,
+            ),
+            github_api_scope: mandatory_string("SERVO_CI_GITHUB_API_SCOPE", github_api_scope),
+            github_api_suffix: mandatory_string("SERVO_CI_GITHUB_API_SUFFIX", github_api_suffix),
+            libvirt_prefix: mandatory_string("SERVO_CI_LIBVIRT_PREFIX", libvirt_prefix),
+            monitor_data_path,
+            monitor_poll_interval: parse_duration_secs(
+                "SERVO_CI_MONITOR_POLL_INTERVAL",
+                monitor_poll_interval,
+            ),
+            api_cache_timeout: parse_duration_secs("SERVO_CI_API_CACHE_TIMEOUT", api_cache_timeout),
+            monitor_start_timeout: parse_duration_secs(
+                "SERVO_CI_MONITOR_START_TIMEOUT",
+                monitor_start_timeout,
+            ),
+            monitor_reserve_timeout: parse_duration_secs(
+                "SERVO_CI_MONITOR_RESERVE_TIMEOUT",
+                monitor_reserve_timeout,
+            ),
+            monitor_thread_send_timeout: parse_duration_secs(
+                "SERVO_CI_MONITOR_THREAD_SEND_TIMEOUT",
+                monitor_thread_send_timeout,
+            ),
+            monitor_thread_recv_timeout: parse_duration_secs(
+                "SERVO_CI_MONITOR_THREAD_RECV_TIMEOUT",
+                monitor_thread_recv_timeout,
+            ),
+            destroy_all_non_busy_runners: parse_bool(
+                "SERVO_CI_DESTROY_ALL_NON_BUSY_RUNNERS",
+                destroy_all_non_busy_runners,
+            ),
+            dont_register_runners: parse_bool(
+                "SERVO_CI_DONT_REGISTER_RUNNERS",
+                dont_register_runners,
+            ),
+            dont_create_runners: parse_bool("SERVO_CI_DONT_CREATE_RUNNERS", dont_create_runners),
+            main_repo_path: mandatory_string("SERVO_CI_MAIN_REPO_PATH", main_repo_path),
+        };
+
+        result.validate()
+    }
+
+    fn validate(self) -> Self {
+        if self.monitor_api_token_authorization_value == "Bearer ChangeMe" {
+            panic!("SERVO_CI_MONITOR_API_TOKEN must be changed!");
+        }
+
+        self
+    }
+
+    fn monitor_api_token_authorization_value(monitor_api_token: &str) -> String {
+        format!("Bearer {monitor_api_token}")
     }
 }
 
@@ -104,18 +226,29 @@ impl Toml {
         File::open(path)?.read_to_string(&mut result)?;
         let result: Toml = toml::from_str(&result)?;
 
-        if !result.external_base_url.ends_with("/") {
+        result.validate()
+    }
+
+    #[cfg(any(test, feature = "test"))]
+    fn load_for_tests() -> eyre::Result<Self> {
+        let result: Toml = toml::from_str(include_str!("../../monitor.toml.example"))?;
+
+        result.validate()
+    }
+
+    fn validate(self) -> eyre::Result<Self> {
+        if !self.external_base_url.ends_with("/") {
             bail!("external_base_url setting must end with slash!");
         }
 
-        for (key, profile) in result.profiles.iter() {
+        for (key, profile) in self.profiles.iter() {
             assert_eq!(
                 *key, profile.base_vm_name,
                 "Runner::base_vm_name relies on Toml.profiles key (profile name) and base_vm_name being equal"
             );
         }
 
-        Ok(result)
+        Ok(self)
     }
 
     pub fn base_image_max_age(&self) -> Duration {
@@ -140,19 +273,31 @@ fn env_option_string(key: &str) -> Option<String> {
 }
 
 fn env_string(key: &str) -> String {
-    env_option_string(key).expect(&format!("{key} not defined!"))
+    mandatory_string(key, env_option_string(key))
 }
 
-fn env_u64(key: &str) -> u64 {
-    env_string(key)
+fn mandatory_string(key: &str, value: Option<String>) -> String {
+    value.expect(&format!("{key} not defined!"))
+}
+
+fn parse_u64(key: &str, value: Option<String>) -> u64 {
+    mandatory_string(key, value)
         .parse()
         .expect(&format!("Failed to parse {key}!"))
 }
 
 fn env_duration_secs(key: &str) -> Duration {
-    Duration::from_secs(env_u64(key))
+    parse_duration_secs(key, env_option_string(key))
+}
+
+fn parse_duration_secs(key: &str, value: Option<String>) -> Duration {
+    Duration::from_secs(parse_u64(key, value))
 }
 
 fn env_bool(key: &str) -> bool {
-    env::var_os(key).is_some()
+    parse_bool(key, env::var_os(key).map(|_| "".to_owned()))
+}
+
+fn parse_bool(_key: &str, value: Option<String>) -> bool {
+    value.is_some()
 }
