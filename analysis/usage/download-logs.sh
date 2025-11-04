0fbc443d8c9c9f5c3006d024123a721b6d8b59a5
Delan Azabani <dazabani@igalia.com>
2025-11-04 14:51:05 +0800

    analysis: Usage report for 2025-11-04

--- analysis/usage/download-logs.sh
+++ analysis/usage/download-logs.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
-# Usage: download-logs.sh <host ...>
+# Usage: download-logs.sh <host ...> [-- extra journalctl options]
 #   e.g. download-logs.sh ci{0..4}.servo.org
-for host in "$@"; do
-    ssh "root@$host" 'journalctl -u monitor | dd bs=1M status=progress | zstd' > "$host.log.zst"
+set -eu
+double_dash=false
+hosts=
+# Remove hosts and double dash if any
+for arg in "$@"; do
+    shift
+    if [ "$arg" = -- ]; then
+        double_dash=true
+        break
+    else
+        hosts=$hosts${hosts:+ }$arg
+    fi
+done
+# Escape the extra journalctl options
+for arg in "$@"; do
+    shift
+    set -- "$@" "$(printf \%s "$arg" | sed 's/./\\&/g')"
+done
+set -x
+
+for host in $hosts; do
+    ssh "root@$host" journalctl -u monitor "$@" \| dd bs=1M status=progress \| zstd > "$host.log.zst"
     unzstd "$host.log.zst"
 done
