af78f981437d30cc00ee0efaf7918345fdaf187b
Narfinger <Narfinger@users.noreply.github.com>
2026-01-20 11:37:39 +0100

    do not print out so many messages
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- docker/runner/run_mitmproxy_and_gh.sh
+++ docker/runner/run_mitmproxy_and_gh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uv tool run --from mitmproxy mitmdump -v --server-replay /mitmdump/dump-current --set server_replay_extra=404 --set server_replay_reuse=true &
+uv tool run --from mitmproxy mitmdump --server-replay /mitmdump/dump-current --set server_replay_extra=404 --set server_replay_reuse=true &
 
 # we only ever use "--jitconfig ..." so two arguments is enough.
 /home/servo_ci/runner/run.sh $1 $2 &
