dd3c36383c04a6a6ef945b6ccc1a8f958612b134
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2026-04-30 14:28:13 +0200

    docker: Bump actions runner version to 2.334 (#124)

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 set -eu
 
 SERVO_GIT_HASH=$(git ls-remote https://github.com/servo/servo.git --branches refs/heads/main | awk '{ print $1}')
-GITHUB_ACTIONS_RUNNER_VERSION="2.333.0"
+GITHUB_ACTIONS_RUNNER_VERSION="2.334.0"
 MITMPROXY_VERSION="12.2.1"
 RUST_VERSION="1.92.0"
 UV_VERSION="0.9.28"
