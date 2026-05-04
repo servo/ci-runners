fa0dfec9e83a5af8e943c833c907ac21e2fa0957
Jane <5373400+janeoa@users.noreply.github.com>
2026-05-04 13:06:07 +0200

    Add optinal SDK path arg to the build.sh
    Signed-off-by: Jane <5373400+janeoa@users.noreply.github.com>

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,54 @@
 
 set -eu
 
+COMMANDLINE_TOOLS_PATH="https://repo.huaweicloud.com/harmonyos/ohpm/5.1.0/commandline-tools-linux-x64-5.1.0.840.zip"
+STAGED_COMMANDLINE_TOOLS_PATH=
+
+usage() {
+    echo "Usage: $0 [--sdk-path PATH]"
+}
+
+while [[ "$#" -gt 0 ]]
+do
+    case "$1" in
+        --sdk-path)
+            if [[ "$#" -lt 2 ]]
+            then
+                echo "Missing value for --sdk-path" >&2
+                usage >&2
+                exit 1
+            fi
+            COMMANDLINE_TOOLS_PATH="$2"
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown argument: $1" >&2
+            usage >&2
+            exit 1
+            ;;
+    esac
+done
+
+cleanup() {
+    if [[ -n "${STAGED_COMMANDLINE_TOOLS_PATH}" && -f "${STAGED_COMMANDLINE_TOOLS_PATH}" ]]
+    then
+        rm -f "${STAGED_COMMANDLINE_TOOLS_PATH}"
+    fi
+}
+
+trap cleanup EXIT
+
+if [[ -f "${COMMANDLINE_TOOLS_PATH}" ]]
+then
+    STAGED_COMMANDLINE_TOOLS_PATH="hos_commandline_tools/.commandline-tools.zip"
+    cp "${COMMANDLINE_TOOLS_PATH}" "${STAGED_COMMANDLINE_TOOLS_PATH}"
+    COMMANDLINE_TOOLS_PATH=".commandline-tools.zip"
+fi
+
 SERVO_GIT_HASH=$(git ls-remote https://github.com/servo/servo.git --branches refs/heads/main | awk '{ print $1}')
 GITHUB_ACTIONS_RUNNER_VERSION="2.334.0"
 MITMPROXY_VERSION="12.2.1"
@@ -22,7 +70,8 @@ docker build gh_runner -f gh_runner/Dockerfile -t "servo_gha_runner:${GITHUB_ACT
     --build-arg=USERNAME=${IMAGE_USERNAME} \
     --build-arg=GITHUB_ACTIONS_RUNNER_VERSION=${GITHUB_ACTIONS_RUNNER_VERSION}
 docker build hos_commandline_tools -f hos_commandline_tools/Dockerfile -t "hos_commandline_tools" \
-   --build-arg=USERNAME=${IMAGE_USERNAME}
+   --build-arg=USERNAME=${IMAGE_USERNAME} \
+   "--build-arg=COMMANDLINE_TOOLS_PATH=${COMMANDLINE_TOOLS_PATH}"
 
 # Build the actual images
 
